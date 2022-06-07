db73b01ea26f187cd0d80342e2edbb69946bb7f4
Dan Pina <dan@Duclearc-MBP.local>
2022-06-07 16:56:35 +0200

    [main] reordered methods alphabetically

--- wps.ts
+++ wps.ts
@@ -22,107 +22,6 @@ class WPS {
         this.imageType = config.imageType
     }
 
-    /** Runs WPS with the given arguments. */
-    async run(): Promise<void> {
-        const args = this.processArgs()
-        this.setArgs(args)
-        if (!this.targetWebsite) throw new Error('Please specify a target website in ./config.json')
-        const browser = await this.startBrowser()
-        const initialReport: InitialReport = await this.getTargetScore(browser)
-        const formattedReport: FormattedReport = await this.formatReport(initialReport)
-        this.saveToFolder(formattedReport, this.logFilePath)
-        if (args.screenshot) await this.takeScreenshot(browser)
-        console.log(
-            '\n===== YOUR LIGHTHOUSE SCORES =====\n',
-            formattedReport,
-            '\n=================================='
-        )
-        await browser.close()
-    }
-
-    /** Sets the data as defined in the `config.json` */
-    setArgs(args: Args) {
-        if (args.target) this.targetWebsite = (args.target as string)
-        if (args.fullDate) this.fullDate = (args.fullDate as boolean)
-    }
-
-    /** Takes screenshot and saves it to the `screenshots/` folder */
-    async takeScreenshot(browser: Browser) {
-        const page = await browser.newPage()
-        await page.goto(this.targetWebsite)
-        let domain: string = new URL(this.targetWebsite).host
-        domain = domain.includes('www.') ? domain.split('www.')[1].split('.')[0] : domain.split('.')[0]
-        await page.screenshot({
-            path: `${this.screenshotFilePath}${this.getCurrentFormattedDate()}_${domain}.${this.imageType}`
-            // './screenshots/YYYY.MM.DD-HH:mm:ss_google.png'
-        })
-    }
-
-    /** Gets the args (if any) and formats them into an object. */
-    processArgs(): Args {
-        const argsArray = process.argv.slice(2)
-        const argsObj: { [key: string]: any } = {};
-        if (argsArray) {
-            argsArray.map((value: string) => {
-                const separator = value.includes('=') ? '=' : ':'
-                const [key, ...val] = value.split(separator)
-                if (val.length) argsObj[key] = val.join(separator)
-                else argsObj[key] = true
-            })
-        }
-        return argsObj
-    }
-
-    /**
-     * Use Puppeteer to launch headful Chrome and use the default viewport set in `config.json`.
-     */
-    async startBrowser(): Promise<Browser> {
-        return await puppeteer.launch({
-            headless: false,
-            defaultViewport: this.viewport
-        })
-    }
-
-    /**
-     * Lighthouse will open the URL.
-     * Puppeteer will observe `targetWebsite` and return the report.
-     *
-     * @param browser
-     * @returns
-     */
-    async getTargetScore(browser: Browser): Promise<InitialReport> {
-        // Adapted from original at: https://github.com/GoogleChrome/lighthouse/blob/master/docs/puppeteer.md#option-1-launch-chrome-with-puppeteer-and-handoff-to-lighthouse
-
-        const flags: Flags = {
-            port: +(new URL(browser.wsEndpoint())).port,
-            output: 'json',
-            logLevel: 'info',
-            screenEmulation: {
-                disabled: true
-            }
-        }
-        const { lhr } = await lighthouse(this.targetWebsite, flags)
-
-        const report: InitialReport = lhr.categories
-
-        return report
-    }
-
-    /** Returns the current date formatted as `YYYY.MM.DD-HH:mm:ss` */
-    getCurrentFormattedDate(full = this.fullDate): string {
-        const today = new Date()
-        const day = today.getDate().toString().length === 2 ? today.getDate() : `0${today.getDate()}`
-        const month = today.getMonth().toString().length === 2 ? today.getMonth() : `0${today.getMonth()}`
-        const year = today.getFullYear()
-        const hour = today.getHours()
-        const minutes = today.getMinutes()
-        const seconds = today.getSeconds()
-
-        const baseDate = `${year}.${month}.${day}`
-
-        return full ? `${baseDate}-${hour}:${minutes}:${seconds}` : baseDate
-    }
-
     /** Transforms/filters the `initialReport` type into `FormattedReport` */
     async formatReport(initialReport: InitialReport): Promise<FormattedReport> {
 
@@ -144,6 +43,79 @@ class WPS {
 
     }
 
+    /** Returns the current date formatted as `YYYY.MM.DD-HH:mm:ss` */
+    getCurrentFormattedDate(full = this.fullDate): string {
+        const today = new Date()
+        const day = today.getDate().toString().length === 2 ? today.getDate() : `0${today.getDate()}`
+        const month = today.getMonth().toString().length === 2 ? today.getMonth() : `0${today.getMonth()}`
+        const year = today.getFullYear()
+        const hour = today.getHours()
+        const minutes = today.getMinutes()
+        const seconds = today.getSeconds()
+
+        const baseDate = `${year}.${month}.${day}`
+
+        return full ? `${baseDate}-${hour}:${minutes}:${seconds}` : baseDate
+    }
+
+    /**
+     * Lighthouse will open the URL.
+     * Puppeteer will observe `targetWebsite` and return the report.
+     *
+     * @param browser
+     * @returns
+     */
+     async getTargetScore(browser: Browser): Promise<InitialReport> {
+        // Adapted from original at: https://github.com/GoogleChrome/lighthouse/blob/master/docs/puppeteer.md#option-1-launch-chrome-with-puppeteer-and-handoff-to-lighthouse
+
+        const flags: Flags = {
+            port: +(new URL(browser.wsEndpoint())).port,
+            output: 'json',
+            logLevel: 'info',
+            screenEmulation: {
+                disabled: true
+            }
+        }
+        const { lhr } = await lighthouse(this.targetWebsite, flags)
+
+        const report: InitialReport = lhr.categories
+
+        return report
+    }
+
+    /** Gets the args (if any) and formats them into an object. */
+    processArgs(): Args {
+        const argsArray = process.argv.slice(2)
+        const argsObj: { [key: string]: any } = {};
+        if (argsArray) {
+            argsArray.map((value: string) => {
+                const separator = value.includes('=') ? '=' : ':'
+                const [key, ...val] = value.split(separator)
+                if (val.length) argsObj[key] = val.join(separator)
+                else argsObj[key] = true
+            })
+        }
+        return argsObj
+    }
+
+    /** Runs WPS with the given arguments. */
+    async run(): Promise<void> {
+        const args = this.processArgs()
+        this.setArgs(args)
+        if (!this.targetWebsite) throw new Error('Please specify a target website in ./config.json')
+        const browser = await this.startBrowser()
+        const initialReport: InitialReport = await this.getTargetScore(browser)
+        const formattedReport: FormattedReport = await this.formatReport(initialReport)
+        this.saveToFolder(formattedReport, this.logFilePath)
+        if (args.screenshot) await this.takeScreenshot(browser)
+        console.log(
+            '\n===== YOUR LIGHTHOUSE SCORES =====\n',
+            formattedReport,
+            '\n=================================='
+        )
+        await browser.close()
+    }
+
     /** Saves file to folder */
     saveToFolder(file: FormattedReport, folder: string): void {
         try {
@@ -155,6 +127,34 @@ class WPS {
         catch (error) {
             console.log(error)
         }
+    }
+
+    /** Sets the data as defined in the `config.json` */
+    setArgs(args: Args) {
+        if (args.target) this.targetWebsite = (args.target as string)
+        if (args.fullDate) this.fullDate = (args.fullDate as boolean)
+    }
+
+    /**
+     * Use Puppeteer to launch headful Chrome and use the default viewport set in `config.json`.
+     */
+     async startBrowser(): Promise<Browser> {
+        return await puppeteer.launch({
+            headless: false,
+            defaultViewport: this.viewport
+        })
+    }
+
+    /** Takes screenshot and saves it to the `screenshots/` folder */
+    async takeScreenshot(browser: Browser) {
+        const page = await browser.newPage()
+        await page.goto(this.targetWebsite)
+        let domain: string = new URL(this.targetWebsite).host
+        domain = domain.includes('www.') ? domain.split('www.')[1].split('.')[0] : domain.split('.')[0]
+        await page.screenshot({
+            path: `${this.screenshotFilePath}${this.getCurrentFormattedDate()}_${domain}.${this.imageType}`
+            // './screenshots/YYYY.MM.DD-HH:mm:ss_google.png'
+        })
     }
 }
 
